da0d8987fec5ada8f9fd6da0ef4874375c273d08
GabrUelRodrigues <gabruel.rodrigues2001@gmail.com>
2023-07-28 15:32:03 -0300

    Project Setup

---
+++ website/auth.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+
+auth = Blueprint("auth", __name__)
+
+@auth.route("/sign-up")
+def sign_up():
+    return "Sign up"
+
+@auth.route("/login")
+def login():
+    return "Login"
+
+@auth.route("/logout")
+def logout():
+    return "Logout"
